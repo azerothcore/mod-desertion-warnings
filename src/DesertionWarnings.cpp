194e1644ad1c72fcc3add2f8349432ce21382f73
Kargatum <dowlandtop@yandex.com>
2021-11-29 22:45:32 +0700

    feat(Misc): add support new api (#3)

--- src/DesertionWarnings.cpp
+++ src/DesertionWarnings.cpp
@@ -1,25 +1,39 @@
+/*
+ * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Config.h"
 #include "Chat.h"
 
-#define DESERTION_ENABLED_CONFIG "DesertionWarnings.Enabled"
-#define WARNING_TEXT_CONFIG "DesertionWarnings.WarningText"
-
-class DesertionWarnings : public PlayerScript {
+class DesertionWarnings : public PlayerScript
+{
 public:
-
     DesertionWarnings() : PlayerScript("DesertionWarnings") { }
 
     void OnBattlegroundDesertion(Player* player, const BattlegroundDesertionType type) override
     {
-        if (sConfigMgr->GetBoolDefault(DESERTION_ENABLED_CONFIG, false))
+        if (sConfigMgr->GetOption<bool>("DesertionWarnings.Enabled", false))
         {
             switch (type)
             {
                 case BG_DESERTION_TYPE_LEAVE_QUEUE:
                 case BG_DESERTION_TYPE_NO_ENTER_BUTTON:
-                    ChatHandler(player->GetSession()).PSendSysMessage("%s", sConfigMgr->GetStringDefault(WARNING_TEXT_CONFIG, "").c_str());
+                    ChatHandler(player->GetSession()).PSendSysMessage("%s", sConfigMgr->GetOption<std::string>("DesertionWarnings.WarningText", "").c_str());
                     break;
                 default:
                     break;
@@ -28,7 +42,8 @@ public:
     }
 };
 
-void AddDesertionWarningsScripts() {
+void AddDesertionWarningsScripts()
+{
     new DesertionWarnings();
 }
 
